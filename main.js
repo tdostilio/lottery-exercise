b7ccad68496bc8d24ffaf83d745c6e61fec94710
tdostilio <tdostilio@gmail.com>
2017-07-19 13:11:33 -0400

    added create array function and remove character

--- main.js
+++ main.js
@@ -6,16 +6,36 @@ var LOTTERY_SELECTOR = '[data-type="lottery-container"]';
 
 function generateNumber69(element) {
     element.textContent = Math.floor(Math.random()*69);
+
 }
 
 function generateNumber26(element) {
     element.textContent = Math.floor(Math.random()*26);
 }
 
+function removeNumber(remove, array) {
+    array.splice(remove-1,-1)
+    return array;
+}
+
+
+function createArray(minNumber, maxNumber) {
+    var newArray = [];
+    for (var i=minNumber; i <=maxNumber; i++) {
+        newArray.push(i);
+    }
+    return newArray;
+}
+
+
+
 function generateLotto() {
     var firstFive = document.querySelectorAll(FIRST_FIVE_NUMBER_SELECTOR);
+    var array1 = createArray(1,69)
     firstFive.forEach(function (element) {
         generateNumber69(element);
+
+
     })
     var lastTwo = document.querySelectorAll(LAST_TWO_NUMBER_SELECTOR);
     lastTwo.forEach(function (element) {
