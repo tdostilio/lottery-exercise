70015e85c98aeb2ec0d595aa1f57b24da1c7072b
tdostilio <tdostilio@gmail.com>
2017-07-20 13:56:40 -0400

    added ball animation and new color scheme

--- main.js
+++ main.js
@@ -7,32 +7,27 @@ var LOTTERY_SELECTOR = '[data-type="lottery-container"]';
 function generateNumber69(element, array) {
     var number = Math.floor(Math.random()*array.length)+1;
     if (array.includes(number)) {
-        // console.log(number + "is in array");
         element.textContent = number;
         return number;
     } else {
-        // console.log('not in the array, re-running');
         generateNumber69(element, array);
     }
-    console.log(number + " number");
-    // removeNumber(arraylocation,array);
-    console.log(array);
-
 }
 
-
-
-function generateNumber26(element) {
-    element.textContent = Math.floor(Math.random()*26);
+function generateNumber26(element, array) {
+    var number = Math.floor(Math.random()*array.length)+1;
+    if (array.includes(number)) {
+        element.textContent = number;
+        return number;
+    } else {
+        generateNumber69(element, array);
+    }
 }
 
 function removeNumber(number, array) {
     var arraylocation = array.indexOf(number);
-    console.log(number + "this is the number");
-    console.log(arraylocation + " array location");
     array.splice(arraylocation,1)
 }
-
 
 function createArray(minNumber, maxNumber) {
     var newArray = [];
@@ -42,27 +37,37 @@ function createArray(minNumber, maxNumber) {
     return newArray;
 }
 
-console.log(createArray(1,69));
-
-
-
 function generateLotto() {
-    var firstFive = document.querySelectorAll(FIRST_FIVE_NUMBER_SELECTOR);
+    var firstFive = $(FIRST_FIVE_NUMBER_SELECTOR);
     var testArray = createArray(1,69);
-    firstFive.forEach(function (element) {
-        console.log(testArray);
+    firstFive.each(function (i,element) {
         var theNumber = generateNumber69(element, testArray);
         removeNumber(theNumber, testArray);
     })
-    var lastTwo = document.querySelectorAll(LAST_TWO_NUMBER_SELECTOR);
+    var lastTwo = $(LAST_TWO_NUMBER_SELECTOR);
     var secondArray = createArray(1,26);
-    lastTwo.forEach(function (element) {
-        generateNumber26(element);
+    lastTwo.each(function (i,element) {
+        var theSecondNumber = generateNumber69(element, secondArray);
+        removeNumber(theSecondNumber, secondArray);
     });
 }
 
-document.querySelector(BUTTON_SELECTOR).addEventListener('click', function() {
+// BUTTON
+$(BUTTON_SELECTOR).on('click', function() {
     generateLotto();
+    $(".power-ball-box").each(function (i,element) {
+      setTimeout(function() {
+        $(element).toggleClass('transformed');  
+      },i*300);
+      
+      
+    //   $(element).toggleClass('transformed');  
+    }) 
 });
 
+// Refresh Button 
+
+$(".refresh-button").on('click', function() {
+    location.reload();
+});
 
